8703aee0c8d8cb820a8cd4ddbbf23fa5dd736775
gkarthik <xraydiffraction@gmail.com>
2020-11-25 15:09:07 -0800

    Fixed syntax

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ args=($@)
 xml=${args[0]}
 runnum=${args[1]}
 
-java -Djava.library.path="/usr/local/lib:/root/lib" Xms2G -Xmx4G -jar /usr/local/lib/beast.jar -beagle_GPU -seed 112358${runnum} -save_every 10000000 -save_stem ${xml}_run${runnum} $xml
+java -Djava.library.path="/usr/local/lib:/root/lib" -Xms2G -Xmx4G -jar /usr/local/lib/beast.jar -beagle_GPU -seed 112358${runnum} -save_every 10000000 -save_stem ${xml}_run${runnum} $xml
