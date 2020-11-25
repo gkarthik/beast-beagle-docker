7f4c20971ea432fb86c3ec0df77cedceb5d8d718
gkarthik <xraydiffraction@gmail.com>
2020-11-25 14:56:24 -0800

    Added library path

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-source ~/.bashrc
-
 args=($@)
 xml=${args[0]}
 runnum=${args[1]}
 
-beast -beagle_GPU -seed 112358${runnum} -save_every 10000000 -save_stem ${xml}_run${runnum} $xml
+java -Djava.library.path="/usr/local/lib:/root/lib" Xms2G -Xmx4G -jar /usr/local/lib/beast.jar -beagle_GPU -seed 112358${runnum} -save_every 10000000 -save_stem ${xml}_run${runnum} $xml
