808186525b233a7090c07576d5c712ae05a1da48
gkarthik <xraydiffraction@gmail.com>
2021-02-15 11:44:04 -0800

    Added GPU order

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ source ~/.bashrc
 args=($@)
 xml=${args[0]}
 runnum=${args[1]}
+gpunum=${args[2]}
 
-java -Djava.library.path="/usr/local/lib:/root/lib" -Xms2G -Xmx4G -jar /usr/local/lib/beast.jar -beagle_GPU -seed 112358${runnum} -save_every 10000000 -save_stem ${xml}_run${runnum} $xml
+java -Djava.library.path="/usr/local/lib:/root/lib" -Xms2G -Xmx4G -jar /usr/local/lib/beast.jar -beagle_GPU -beagle_order ${gpunum} -seed 112358${runnum} -save_every 10000000 -save_stem ${xml}_run${runnum} $xml
